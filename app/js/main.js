403e63a30836d3063e9578a956c28c79091876e6
iryna.khersonna <iryna.khersonna@Irynas-MacBook-Pro.local>
2020-04-13 19:30:39 +0300

    added new items page

--- app/js/main.js
+++ app/js/main.js
@@ -13,6 +13,27 @@ $('.product-slider__inner').slick({
     slidesToScroll:4,
 });
 
+   $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0 ,
+        to: 600,  
+   });
+
+   $('.icon-th-list').on('click', function(){
+        $('.product__item').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th-large').removeClass('active');
+   });
+
+  $('.icon-th-large').on('click', function () {
+      $('.product__item').removeClass('list');
+  $('.icon-th-large').addClass('active');
+  $('.icon-th-list').removeClass('active');
+
+
+  });
 var mixer = mixitup('.products__inner-box');
  
 
