0e88e5665f962caca969a09b92b6bb1510d5855e
iryna.khersonna <iryna.khersonna@Irynas-MacBook-Pro.local>
2020-04-20 22:48:15 +0300

    added pop-up login

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,39 @@
 let gulp = require('gulp'),
     sass = require('gulp-sass'),
     rename = require('gulp-rename'),
-    browserSync =require('browser-sync'),
+    browserSync = require('browser-sync'),
     autoprefixer = require('gulp-autoprefixer'),
     concat = require('gulp-concat'),
-    uglify= require('gulp-uglify');
-    cssmin = require('gulp-cssmin')
+    uglify = require('gulp-uglify');
+cssmin = require('gulp-cssmin')
 
 
-gulp.task('sass', function(){
-   return gulp.src('app/scss/**/*.scss')
-            .pipe(sass({outputStyle: 'expand'}))
-            .pipe(rename({suffix: '.min'}))
-            .pipe(autoprefixer({
-                overrideBrowserslist:['last 8 versions']
-            }))
-            .pipe(gulp.dest('app/css'))
-            .pipe(browserSync.reload({stream: true}))
-}); 
+gulp.task('sass', function () {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass({
+            outputStyle: 'expand'
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 8 versions']
+        }))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
 
-gulp.task('style', function(){
+gulp.task('style', function () {
     return gulp.src([
-        'node_modules/normalize.css/normalize.css',
-        'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css',
-        'node_modules/rateyo/src/jquery.rateyo.css',
-        'node_modules/ion-rangeslider/css/ion.rangeSlider.css'
-    ])
+            'node_modules/normalize.css/normalize.css',
+            'node_modules/slick-carousel/slick/slick.css',
+            'node_modules/magnific-popup/dist/magnific-popup.css',
+            'node_modules/rateyo/src/jquery.rateyo.css',
+            'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
+            'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css'
+        ])
 
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -34,14 +41,15 @@ gulp.task('style', function(){
 });
 
 
-gulp.task('script', function(){
+gulp.task('script', function () {
     return gulp.src([
-        'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-        'node_modules/mixitup/dist/mixitup.js',
-        'node_modules/rateyo/src/jquery.rateyo.js',
-        'node_modules/ion-rangeslider/js/ion.rangeSlider.js'
-    ])
+            'node_modules/slick-carousel/slick/slick.js',
+            'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+            'node_modules/mixitup/dist/mixitup.js',
+            'node_modules/rateyo/src/jquery.rateyo.js',
+            'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+            'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'
+        ])
 
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
@@ -49,18 +57,22 @@ gulp.task('script', function(){
 });
 
 
-gulp.task('html', function(){
+gulp.task('html', function () {
     return gulp.src('app/*.html')
-    .pipe(browserSync.reload({stream: true}))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
-gulp.task('js', function(){
+gulp.task('js', function () {
     return gulp.src('app/js/*.js')
-    .pipe(browserSync.reload({stream: true}))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     browserSync.init({
         server: {
             baseDir: "app/"
@@ -68,7 +80,7 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function () {
     gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
     gulp.watch('app/*.html', gulp.parallel('html'))
     gulp.watch('app/js/*.js', gulp.parallel('js'))
