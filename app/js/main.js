22e213984f751f7699f615cc63fbc3a666a010c9
iryna.khersonna <iryna.khersonna@Irynas-MacBook-Pro.local>
2020-04-20 13:24:39 +0300

    added different stages of menu, added responsive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,40 +1,51 @@
-$(function(){
+$(function () {
 
-$(".rate-star").rateYo({
-    rating: 5,
-    starWidth:"12px",
-    readOnly: true,
-});
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true,
+    });
 
-$('.product-slider__inner').slick({
-    dots:true,
-    arrows:false,
-    slidesToShow:4,
-    slidesToScroll:4,
-});
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
 
-   $(".js-range-slider").ionRangeSlider({
+    $(".js-range-slider").ionRangeSlider({
         type: "double",
         min: 0,
         max: 1000,
-        from: 0 ,
-        to: 600,  
-   });
+        from: 0,
+        to: 600,
+    });
 
-   $('.icon-th-list').on('click', function(){
+    $('.icon-th-list').on('click', function () {
         $('.product__item').addClass('list');
-    $('.icon-th-list').addClass('active');
-    $('.icon-th-large').removeClass('active');
-   });
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+    });
 
-  $('.icon-th-large').on('click', function () {
-      $('.product__item').removeClass('list');
-  $('.icon-th-large').addClass('active');
-  $('.icon-th-list').removeClass('active');
+    $('.icon-th-large').on('click', function () {
+        $('.product__item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
+
+    });
+
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').slideToggle();
+    });
+
+    $('.header__btn-menu').on('click', function () {
+        $('.header__box').toggleClass('active');
+    });
 
 
-  });
-var mixer = mixitup('.products__inner-box');
- 
+
+
+    var mixer = mixitup('.products__inner-box');
+
 
 });
