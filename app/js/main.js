93620a89c675313879727de98c8b6eb73e90eaea
iryna.khersonna <iryna.khersonna@Irynas-MacBook-Pro.local>
2020-05-10 18:15:31 +0300

    added adaptiv

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,30 @@ $(function () {
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4,
+        responsive: [{
+                breakpoint: 1900,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1441,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 801,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
     });
     // range slider
     $(".js-range-slider").ionRangeSlider({
